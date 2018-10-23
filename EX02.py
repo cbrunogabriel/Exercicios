12212eabe1702d5e0453e74792f7f6c0dbac8289
brunogabriel/poo-unis-ead <39252380+cbrunogabriel@users.noreply.github.com>
2018-10-23 18:02:45 -0300

    Add files via upload

---
+++ EX02.py
@@ -0,0 +1,25 @@
+﻿print('Exercicios-02')
+print('')
+
+def Triangular(i,x):
+	if(x > 0):
+		while(i <= x):
+			i2 = i+1
+			i3 = i+2
+			valor = i *i2 *i3
+			i = i + 1
+			if(valor == x):
+				return ('Numero '+str(x)+' é triangular. Resultado de: '+ str(i-1)+'*'+str(i2)+'*'+str(i3))
+			if(resultado > x):
+				break
+		return 'Numero '+str(x)+' não é triangular'
+	else:
+		return 'Numero Negativo Invalido'
+
+print(Triangular(1,8))
+print(Triangular(1, 25))
+print(Triangular(1, 58))
+print(Triangular(1, 120))
+print(Triangular(1, 334))
+print(Triangular(1, 122))
+print(Triangular(1, -11))
