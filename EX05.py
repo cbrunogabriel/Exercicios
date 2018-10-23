2fba1889e65d1cd8cc9b695184ff19d8e2d221d8
brunogabriel/poo-unis-ead <39252380+cbrunogabriel@users.noreply.github.com>
2018-10-23 17:51:11 -0300

    EXs

---
+++ EX05.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+
+    if n < 2:
+        return False
+
+    for i in range(2, n):
+        if not n % i:
+            return False
+    else:
+        return True
+
+# Para x de 1 a 100
+for x in range(1, 101):
+    if is_prime(x):
+        print x
